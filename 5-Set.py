8a4f5ef4d1f2ccfcac8f53e5cb0ac242e479f44e
blairwillow <pan20072550@gmail.com>
2024-12-15 23:23:40 +0700

    Add files via upload

---
+++ 5-Set.py
@@ -0,0 +1,10 @@
+
+print("โปรแกรมคำนวณหา Set\na={10,20,30,40,50,60}\nb={30,40,50,60,70,80}")
+a={10,20,30,40,50,60}
+b={30,40,50,60,70,80}
+print("ให้นักเรียนเขียนคำสั่งด้วยภาษาไพทอนหาเซตของที่กำหนดให้แสดงผลตามที่กำหนด")
+print(a.union(b))
+print(a.intersection(b))
+print(a.difference(b))
+print(a.symmetric_difference(b))
+print("End")
